f2cba8c1acd1fff67dac4cfb3adea6ef95c22fe8
gotgotaind <gotgotaind@gmail.com>
2021-08-31 00:37:15 +0200

    filename seems ok

--- web_server/web_server.py
+++ web_server/web_server.py
@@ -1,33 +1,39 @@
 #import socket module
 from socket import *
 import sys # In order to terminate the program
+
+serverPort=12005
 serverSocket = socket(AF_INET, SOCK_STREAM)
 #Prepare a sever socket
 #Fill in start
+serverSocket.bind(('',serverPort))
+serverSocket.listen(1)
 #Fill in end
 while True:
-#Establish the connection
-print('Ready to serve...')
-connectionSocket, addr = #Fill in start #Fill in end
-try:
-message = #Fill in start #Fill in end
-filename = message.split()[1]
-f = open(filename[1:])
-outputdata = #Fill in start #Fill in end
-#Send one HTTP header line into socket
-#Fill in start
-#Fill in end
-#Send the content of the requested file to the client
-for i in range(0, len(outputdata)):
-connectionSocket.send(outputdata[i].encode())
-connectionSocket.send("\r\n".encode())
-connectionSocket.close()
-except IOError:
-#Send response message for file not found
-#Fill in start
-#Fill in end
-#Close client socket
-#Fill in start
-#Fill in end
+    #Establish the connection
+    print('Ready to serve...')
+    connectionSocket, addr = serverSocket.accept() #Fill in start #Fill in end
+    try:
+        message = connectionSocket.recv(1024).decode() #Fill in start #Fill in end
+        filename = message.split()[1]
+        print(f'filename requested is {filename}')
+        f = open(filename[1:])
+        outputdata = "zobi" #Fill in start #Fill in end
+        #Send one HTTP header line into socket
+        #Fill in start
+        #Fill in end
+        #Send the content of the requested file to the client
+        for i in range(0, len(outputdata)):
+            connectionSocket.send(outputdata[i].encode())
+            connectionSocket.send("\r\n".encode())
+        connectionSocket.close()
+    except IOError:
+        pass
+        #Send response message for file not found
+        #Fill in start
+        #Fill in end
+        #Close client socket
+        #Fill in start
+        #Fill in end
 serverSocket.close()
 sys.exit()#Terminate the program after sending the corresponding data
